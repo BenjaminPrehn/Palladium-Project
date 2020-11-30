1cc31377cb3d0958687a10d43cd8d50159303b01
BenjaminPrehn <57618388+BenjaminPrehn@users.noreply.github.com>
2020-11-30 10:17:54 +0100

    Færdig opsætning

--- src/main/java/com/managment/palladium/controllers/HomeController.java
+++ src/main/java/com/managment/palladium/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.managment.palladium.Controllers;
+package com.managment.palladium.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
