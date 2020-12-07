0c3671619df9999712394ced440fef9140c62c1c
Emil <emil078a@stud.kea.dk>
2020-12-07 12:29:33 +0100

    fixed chart

--- src/main/java/com/managment/palladium/dto/EmployeeProject.java
+++ src/main/java/com/managment/palladium/dto/EmployeeProject.java
@@ -2,7 +2,7 @@ package com.managment.palladium.dto;
 
 public interface EmployeeProject {
 
-    // Need to have the property names begin with get
+    // Need to have the property names begin with 'get' for Spring Data to be able to read/use it
     public String getFirstName();
     public String getLastName();
     public int getProjectCount();
